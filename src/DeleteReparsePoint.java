b2b9b6d7c4d9822b15946790ccef5d3b836219b5
s3virge <kobzarvit@gmail.com>
2018-10-23 23:32:17 +0300

    was make some changes

--- src/DeleteReparsePoint.java
+++ src/DeleteReparsePoint.java
@@ -4,21 +4,21 @@ import java.io.InputStreamReader;
 
 public class DeleteReparsePoint {
 
-    private File[] listOfFiles; //тут будет список всех файлов
-    private String command;
-    private String folderPath;
-
-    public DeleteReparsePoint(String folderPath) {
-        this.folderPath = folderPath;
-        this.command = "fsutil reparsepoint delete ";
+    public DeleteReparsePoint() {
     }
 
     public static void main(String[] args) {
 
-        DeleteReparsePoint delRepPoint = new DeleteReparsePoint("/mnt/2BFF77C153FC98B1/OneDrive");
+        String folderPath = "/home/s3virge/www";
+//        folderPath = "/mnt/2BFF77C153FC98B1/OneDrive";
 
-      delRepPoint.getListOfFiles();
-      String output = delRepPoint.executeCommand();
+        DeleteReparsePoint delRepPoint = new DeleteReparsePoint();
+
+        File[] listOfFiles = delRepPoint.getListOfFiles(folderPath);
+
+        delRepPoint.executeCommandForEach(listOfFiles, "fsutil reparsepoint delete ");
+
+//        String output = delRepPoint.executeCommand("fsutil reparsepoint delete \"" + listOfFiles[1] + "\"");
 //      System.out.println(output);
 
         //заходим в папку
@@ -30,7 +30,13 @@ public class DeleteReparsePoint {
 
     }
 
-    private String executeCommand() {
+    private final void executeCommandForEach(File[] files, String command) {
+        for (File currentFile : files) {
+            executeCommand(command + "\"" + currentFile.getAbsolutePath() + "\"");
+        }
+    }
+
+    private String executeCommand(String command) {
 
         StringBuffer output = new StringBuffer();
 
@@ -38,8 +44,7 @@ public class DeleteReparsePoint {
         try {
             p = Runtime.getRuntime().exec(command);
             p.waitFor();
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
             String line = "";
             while ((line = reader.readLine())!= null) {
@@ -51,21 +56,22 @@ public class DeleteReparsePoint {
         }
 
         return output.toString();
-
     }
 
-    private void getListOfFiles() {
+    private File[] getListOfFiles(String folderPath) {
         File folder = new File(folderPath);
-        listOfFiles = folder.listFiles();
+        File[] files = folder.listFiles();
 
-//        for (int i = 0; i < listOfFiles.length; i++) {
-//            if (listOfFiles[i].isFile()) {
-//                System.out.println("File " + listOfFiles[i].getName());
-//            }
-//            else if (listOfFiles[i].isDirectory()) {
-//                System.out.println("Directory " + listOfFiles[i].getName());
-//            }
-//        }
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isFile()) {
+                System.out.println("File " + files[i].getName());
+            }
+            else if (files[i].isDirectory()) {
+                System.out.println("Directory " + files[i].getName());
+            }
+        }
+
+        return files;
     }
 
     //@return true if current path has folder
