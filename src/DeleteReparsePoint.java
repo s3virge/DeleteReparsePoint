353f55b56cb50c9d120d9f7bc93dd42f9e916104
s3virge <kobzarvit@gmail.com>
2018-10-23 15:30:06 +0300

    get list of files in selected folder

---
+++ src/DeleteReparsePoint.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+public class DeleteReparsePoint {
+
+    private File[] listOfFiles; //тут будет список всех файлов
+    private String command;
+    private String folderPath;
+
+    public DeleteReparsePoint(String folderPath) {
+        this.folderPath = folderPath;
+        this.command = "fsutil reparsepoint delete ";
+    }
+
+    public static void main(String[] args) {
+
+        DeleteReparsePoint delRepPoint = new DeleteReparsePoint("/mnt/2BFF77C153FC98B1/OneDrive");
+
+        delRepPoint.getListOfFiles();
+
+//        String output = delRepPoint.executeCommand();
+//        System.out.println(output);
+    }
+
+    private String executeCommand() {
+
+        StringBuffer output = new StringBuffer();
+
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine())!= null) {
+                output.append(line + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+
+    }
+
+    private void getListOfFiles() {
+        File folder = new File(folderPath);
+        listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+            }
+            else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+    }
+
+}
