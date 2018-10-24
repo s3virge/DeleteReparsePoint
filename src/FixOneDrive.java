7d36836e5a37965b92ee04bfc80147140f19c31b
S3ViRGE <kobzarvit@gmail.com>
2018-10-24 11:56:17 +0300

    Rename Main Class

--- src/FixOneDrive.java
+++ src/FixOneDrive.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 
-public class DeleteReparsePoint {
+public class FixOneDrive {
 
     public static void main(String[] args) {
 
